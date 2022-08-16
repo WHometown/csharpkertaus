45e535408c051089780b2a2b41225688576522ba
saku.holopainen <saku.holopainen@CZC222BX2N>
2022-08-16 14:07:06 +0300

    Add project files.

---
+++ DateTime/DateTime/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+public class Example
+{
+    public static void Main()
+    {
+        var date1 = DateTime.Now.ToString("s", System.Globalization.CultureInfo.InvariantCulture);
+        Console.WriteLine(date1);
+    }
+}
---
+++ hei_jukka/hei_jukka/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+public class Example
+{
+    public static void Main()
+    {
+        Console.Write("Kirjoita nimesi: ");
+        string name = Console.ReadLine();
+        Console.Write("Hei ");
+        Console.Write(name);
+    }
+}
